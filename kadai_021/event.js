91983283307a43e2cca6dc4b5585d9bd7584c6cc
kotosakimakoto <147785307+kotosakimakoto@users.noreply.github.com>
2023-11-23 20:06:28 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -2,8 +2,9 @@ const btn = document.getElementById('btn');
 
 const text = document.getElementById('text');
 
-  btn.addEventListener('click', () => {;
+  btn.addEventListener('click', () => {
       setTimeout(() => { 
         text.textContent = 'ボタンをクリックしました';  
       }, 2000);
  })
+
